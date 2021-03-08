6de01ce7a4d74242b6b7ebc3a02d160697e87ab8
CasperLC <43999061+CasperLC@users.noreply.github.com>
2021-03-08 11:57:00 +0100

    4 SPs added

--- DBAssignment/Program.cs
+++ DBAssignment/Program.cs
@@ -13,33 +13,154 @@ namespace DBAssignment
             // Create a Connection Object
             string ConnectionString = "Integrated Security=SSPI;" + "Initial Catalog=Company;" + "Data Source = localhost;";
 
-            using (SqlConnection connection =
-            new SqlConnection(ConnectionString))
-            {
-                SqlCommand cmd = new SqlCommand("TestSP123", connection);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Add("@DepartmentNumber", SqlDbType.Int).Value = 4;
+            //createDepartmentProcedure(ConnectionString, "TestingDepartment333", 333445555);
 
-                Console.WriteLine("\n\nStored Procedure 1");
+            //updateDepartmentName(ConnectionString, 12, "UpdatedDepartment");
+
+            //updateDepartmentManager(ConnectionString, 12, 453453453);
+
+            //deleteDepartmentProcedure(ConnectionString, 9);
+
+            Console.ReadLine();
+        }
+
+        private static void createDepartmentProcedure(string ConnectionString, string DName, decimal MgrSSN)
+        {
+           using (SqlConnection connection =
+           new SqlConnection(ConnectionString))
+            {
+                // Creating SQL Commands
+                SqlCommand cmd = new SqlCommand("usp_CreateDepartment", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@DName", SqlDbType.NVarChar).Value = DName;
+                cmd.Parameters.Add("@MgrSSN", SqlDbType.Decimal).Value = MgrSSN;
+
+                string queryString = "SELECT DName, MgrSSN FROM  Department";
+
+                SqlCommand cmdAfter = new SqlCommand(queryString, connection);
+
+
+                Console.WriteLine("\n\nStored Procedure: CreateDepartment(DName, MgrSSN)");
                 //Each reader[x] reads 1 column, so it needs to be equal to the columns selected
                 try
                 {
                     connection.Open();
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    while (reader.Read())
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataReader reader2 = cmdAfter.ExecuteReader();
+                    while (reader2.Read())
                     {
                         Console.WriteLine("{0}\t{1}",
-                            reader[0], reader[1]);
+                            reader2[0], reader2[1]);
                     }
-                    reader.Close();
+                    reader2.Close();
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
             }
+        }
 
-            Console.ReadLine();
+        private static void updateDepartmentName(string ConnectionString, int DNumber, string DName)
+        {
+           using (SqlConnection connection =
+           new SqlConnection(ConnectionString))
+            {
+                // Creating SQL Commands
+                SqlCommand cmd = new SqlCommand("usp_UpdateDepartmentName", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@DName", SqlDbType.NVarChar).Value = DName;
+                cmd.Parameters.Add("@DNumber", SqlDbType.Int).Value = DNumber;
+
+                string queryString = "SELECT DName FROM  Department";
+
+                SqlCommand cmdAfter = new SqlCommand(queryString, connection);
+
+
+                Console.WriteLine("\n\nStored Procedure: UpdateDepartmentName(DNumber, DName)");
+                //Each reader[x] reads 1 column, so it needs to be equal to the columns selected
+                try
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataReader reader2 = cmdAfter.ExecuteReader();
+                    while (reader2.Read())
+                    {
+                        Console.WriteLine("{0}",
+                            reader2[0]);
+                    }
+                    reader2.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void updateDepartmentManager(string ConnectionString, int DNumber, decimal MgrSSn)
+        {
+            using (SqlConnection connection =
+           new SqlConnection(ConnectionString))
+            {
+                // Creating SQL Commands
+                SqlCommand cmd = new SqlCommand("usp_UpdateDepartmentManager", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@MgrSSN", SqlDbType.Decimal).Value = MgrSSn;
+                cmd.Parameters.Add("@DNumber", SqlDbType.Int).Value = DNumber;
+
+                string queryString = "SELECT DName, MgrSSN FROM  Department";
+
+                SqlCommand cmdAfter = new SqlCommand(queryString, connection);
+
+
+                Console.WriteLine("\n\nStored Procedure: UpdateDepartmentManager(DNumber, MgrSSN)");
+                //Each reader[x] reads 1 column, so it needs to be equal to the columns selected
+                try
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+
+                    SqlDataReader reader2 = cmdAfter.ExecuteReader();
+                    while (reader2.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}",
+                             reader2[0], reader2[1]);
+                    }
+                    reader2.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void deleteDepartmentProcedure(string ConnectionString, int DNumber)
+        {
+            using (SqlConnection connection =
+            new SqlConnection(ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("usp_DeleteDepartment", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@DNumber", SqlDbType.Int).Value = DNumber;
+
+                Console.WriteLine("\n\nStored Procedure DeleteDepartment");
+                //Each reader[x] reads 1 column, so it needs to be equal to the columns selected
+                try
+                {
+                    connection.Open();
+                    var result = cmd.ExecuteNonQuery();
+                    Console.WriteLine("Department number " + DNumber + " was deleted");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
+
 }
